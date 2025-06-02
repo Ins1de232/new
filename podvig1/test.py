08350652244c11359a7e38b4d042b429b49f71e1
Mubarak179 <Bobirut97@gmail.com>
2025-06-02 08:58:20 +0000

    podvig1 и podvig2

---
+++ podvig1/test.py
@@ -0,0 +1,7 @@
+def get_sq(x):
+    return x ** 2  
+
+
+number = float(input())
+result = get_sq(number)
+print(result)       
---
+++ podvig2/py.py
@@ -0,0 +1,2 @@
+def triugol (a, b, c):
+    return a < b + c and b < a + c and c < a + b
